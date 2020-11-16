7833b7c7cfd583d8a9be5bc66734d00d5b6f6185
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:38:01 -0500

    Update DriverInfo.java

--- src/main/java/com/smidur/aventon/models/DriverInfo.java
+++ src/main/java/com/smidur/aventon/models/DriverInfo.java
@@ -1,8 +1,11 @@
 package com.smidur.aventon.models;
 
-/**
- * Created by marqueg on 7/4/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class DriverInfo {
     private String plates;
     private String makeModel;
