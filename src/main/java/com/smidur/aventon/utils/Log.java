4d077b31b27d3bf38a825eb320ce7ea799429d33
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:43:06 -0500

    Update Log.java

--- src/main/java/com/smidur/aventon/utils/Log.java
+++ src/main/java/com/smidur/aventon/utils/Log.java
@@ -6,9 +6,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-/**
- * Created by marqueg on 4/12/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class Log {
 
     private static Logger logger;
