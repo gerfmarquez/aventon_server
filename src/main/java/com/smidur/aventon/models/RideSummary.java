4f4fa2de4e498d6606c50625ade50404204200a3
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:40:01 -0500

    Update RideSummary.java

--- src/main/java/com/smidur/aventon/models/RideSummary.java
+++ src/main/java/com/smidur/aventon/models/RideSummary.java
@@ -1,8 +1,11 @@
 package com.smidur.aventon.models;
 
-/**
- * Created by marqueg on 8/14/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class RideSummary {
 
     private String passengerId;
