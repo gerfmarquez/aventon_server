525d827dcc9e746a52e5536031ec5a152d842877
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:41:24 -0500

    Update RideAvailabilityServlet.java

--- src/main/java/com/smidur/aventon/servlets/RideAvailabilityServlet.java
+++ src/main/java/com/smidur/aventon/servlets/RideAvailabilityServlet.java
@@ -23,9 +23,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
 
-/**
- * Created by marqueg on 2/7/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 @WebServlet(asyncSupported = true)
 public class RideAvailabilityServlet extends RootServlet {
 
