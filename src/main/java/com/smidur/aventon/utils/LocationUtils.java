9849bda3c5e4b0dcb8cdf9bda808db596dd4b645
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:42:50 -0500

    Update LocationUtils.java

--- src/main/java/com/smidur/aventon/utils/LocationUtils.java
+++ src/main/java/com/smidur/aventon/utils/LocationUtils.java
@@ -1,8 +1,11 @@
 package com.smidur.aventon.utils;
 
-/**
- * Created by marqueg on 5/17/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class LocationUtils {
 
 
