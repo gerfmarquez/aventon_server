54d1ee0bb40f443f736ea95e0f6228070186775d
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:36:55 -0500

    Update RideManager.java

--- src/main/java/com/smidur/aventon/managers/RideManager.java
+++ src/main/java/com/smidur/aventon/managers/RideManager.java
@@ -16,9 +16,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-/**
- * Created by marqueg on 2/8/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class RideManager {
 
     private static RideManager i;
