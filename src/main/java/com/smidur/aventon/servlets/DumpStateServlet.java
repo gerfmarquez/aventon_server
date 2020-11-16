c7d55c4e3c64cf07a4a4f0e60623edc7442b22a9
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:41:06 -0500

    Update DumpStateServlet.java

--- src/main/java/com/smidur/aventon/servlets/DumpStateServlet.java
+++ src/main/java/com/smidur/aventon/servlets/DumpStateServlet.java
@@ -12,9 +12,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-/**
- * Created by marqueg on 8/29/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class DumpStateServlet extends HttpServlet {
 
 
