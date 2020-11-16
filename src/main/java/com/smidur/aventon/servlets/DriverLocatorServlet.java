182408831412682ecf6c29e8a6c19e9f906cbd16
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:40:52 -0500

    Update DriverLocatorServlet.java

--- src/main/java/com/smidur/aventon/servlets/DriverLocatorServlet.java
+++ src/main/java/com/smidur/aventon/servlets/DriverLocatorServlet.java
@@ -3,9 +3,12 @@ package com.smidur.aventon.servlets;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-/**
- * Created by marqueg on 2/7/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 @WebServlet
 public class DriverLocatorServlet extends HttpServlet {
 }
